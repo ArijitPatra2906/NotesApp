e3839dc6d0541515556361bdc945b6c2fdb76bae
ArijitPatra2906 <patraarijit440@gmail.com>
2024-11-24 23:29:36 +0530

    fix

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -28,6 +28,7 @@ const Dashboard: React.FC = () => {
   const [isDeleting, setIsDeleting] = useState<boolean>(false);
 
   const fetchNotes = async () => {
+    setLoading(true);
     try {
       const response = await instance.get(`/api/note`);
       setNotes(Array.isArray(response.data) ? response.data : []);
@@ -174,7 +175,6 @@ const Dashboard: React.FC = () => {
         </div>
       )}
 
-      {/* Delete Confirmation Modal */}
       {deleteModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg w-96">
@@ -205,8 +205,43 @@ const Dashboard: React.FC = () => {
         </div>
       )}
 
-      {/* Loader */}
-      {loading && (
+      {!loading ? (
+        <div className="w-full max-w-md lg:max-w-lg">
+          <h3 className="text-lg font-medium text-gray-800 mb-4">Notes</h3>
+          <div className="space-y-4">
+            {notes.length > 0
+              ? notes.map((note: any) => (
+                  <div
+                    key={note._id}
+                    className="flex justify-between items-center shadow-2xl bg-white rounded-lg p-4"
+                  >
+                    <p className="text-gray-800">{note.title}</p>
+                    <button
+                      onClick={() => {
+                        setNoteToDelete(note._id);
+                        setDeleteModalOpen(true);
+                      }}
+                      className=""
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                ))
+              : "No notes available."}
+          </div>
+        </div>
+      ) : (
         <div className="flex justify-center items-center w-full max-w-md lg:max-w-lg h-48">
           <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
         </div>
@@ -218,44 +253,6 @@ const Dashboard: React.FC = () => {
           <p>{error}</p>
         </div>
       )}
-
-      <div className="w-full max-w-md lg:max-w-lg">
-        <h3 className="text-lg font-medium text-gray-800 mb-4">Notes</h3>
-        <div className="space-y-4">
-          {notes.length > 0
-            ? notes.map((note: any) => (
-                <div
-                  key={note._id}
-                  className="flex justify-between items-center bg-white shadow-2xl rounded-lg p-4"
-                >
-                  <p className="text-gray-800">{note.title}</p>
-                  <button
-                    onClick={() => {
-                      setNoteToDelete(note._id);
-                      setDeleteModalOpen(true);
-                    }}
-                    className=""
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 00 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m0 0 5.042 1.232"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              ))
-            : "No notes available."}
-        </div>
-      </div>
     </div>
   );
 };
